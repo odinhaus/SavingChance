26dbbfd0049fa0ae83ed905a8c4727b7b6400e71
Bill Blondin <billb@simplicityhealthsystems.com>
2015-08-04 07:09:31 -0500

    initial commit

---
+++ Contact.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Contact : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (this.Request.Form.Count > 0)
+        {
+            var path = Server.MapPath("Subscribers.txt");
+            using (var fs = File.AppendText(path))
+            {
+                foreach (var item in this.Request.Form.AllKeys)
+                {
+                    fs.WriteLine(item + ": " + this.Request.Form[item]);
+                }
+                fs.WriteLine();
+            }
+        }
+    }
+}
---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(function() {
+	window.scrollReveal = new scrollReveal();
+	"use strict";
+	
+	// PreLoader
+	$(window).load(function() {
+		$(".loader").fadeOut(400);
+	});
+
+	// Backstretchs
+	$("#header").backstretch("images/bg.png");
+	$("#services").backstretch("images/bg2.png");
+	
+	// Countdown
+	$('.countdown').downCount({
+		date: '10/01/2015 12:00:00',
+		offset: -6
+	});			
+    
+});
