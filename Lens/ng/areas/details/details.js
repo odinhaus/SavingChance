ddc0641e8426a2261689bdaf112b249a0699c347
Bill Blondin <billb@simplicityhealthsystems.com>
2015-08-20 20:09:46 -0500

    bunch of changes - but have basic approach worked out; masonry tiled landing page - started the JS to position the tiles; starting CSS; simple FB login;

---
+++ Lens/ng/areas/details/details.js
@@ -0,0 +1,3 @@
+﻿sc.controller('sc.Details', ['$scope', '$window', 'windowState', function ($scope, $window, windowState) {
+    $scope.title = "Detail Page";
+}]);
--- Lens/ng/main.js
+++ Lens/ng/main.js
@@ -1,43 +1,23 @@
-﻿var myApp = angular.module('myApp', ['infinite-scroll']);
+﻿var sc = angular.module('sc', ['infinite-scroll', 'ngRoute']);
+var $masonry = null;
 
-myApp.controller('DemoController', function ($scope, $window) {
-    $scope.tiles = [];
-    $scope.tileWidth = 229;
-    $scope.gutterWidth = 30;
-    
-    $scope.loadMore = function () {
-        var rowCount = Math.floor($window.innerWidth / $scope.tileWidth);
-        $scope.bodyWidth = rowCount * $scope.tileWidth;
-        var last = $scope.tiles.length;
-        for (var i = 1; i <= rowCount * 3; i++) {
-            var tile = {
-                image: last + i,
-                title: 'Lorem epsum salts or something',
-                caption: 'E plurubus unum',
-                subTitle: 'All for one'
-            };
-            $scope.tiles.push(tile);
-        }
-    };
-});
-
-myApp.directive('resize', function ($window) {
-    return function (scope, element) {
-        var w = angular.element($window);
-        scope.getWindowDimensions = function () {
-            return {
-                'h': w.height(),
-                'w': w.width()
-            };
-        };
-
-        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
-            var rowCount = Math.floor((newValue.w - 2 * scope.gutterWidth) / scope.tileWidth);
-            scope.bodyWidth = rowCount * scope.tileWidth;
-        }, true);
-
-        w.bind('resize', function () {
-            scope.$apply();
+sc.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/:id', {
+            templateUrl: '/ng/areas/details/details.html',
+            controller: 'sc.Details'
+        }).
+        otherwise({
+            templateUrl: '/ng/areas/main/browse.html',
+            controller: 'sc.Browse'
         });
-    }
-});
+  }]);
+
+//sc.factory('windowState', function($window)
+//{
+//    this.bodyWidth = $window.innerWidth;
+//    this.columnCount = 5;
+//    this.gutterWidth = 0;
+//    return this;
+//})
