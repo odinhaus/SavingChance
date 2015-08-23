39f91843343b6af4a0158f23733b348d46fab923
Bill Blondin <billb@simplicityhealthsystems.com>
2015-08-23 01:08:23 -0500

    more browsing changes; improved style; beginnings of showing campaign details;

--- Lens/ng/areas/main/browse.js
+++ Lens/ng/areas/main/browse.js
@@ -2,138 +2,234 @@
     $scope.tiles = [];
     $scope.grid = [];
     $scope.columnCount = 8;
-    $scope.lastRow = addRow($scope.columnCount);
-    $scope.grid.push($scope.lastRow);
+
+    var w = angular.element($window);
+    updateWindowDimensions(w);
+   
+    w.bind('resize', function () {
+        $scope.updateTiles(updateWindowDimensions(w));
+        $scope.$apply();
+    });
+
+    function updateWindowDimensions(w)
+    {
+        var scrollbarAdjust = 0;
+        if ($scope.grid.length == 0)
+        {
+            scrollbarAdjust = getScrollBarWidth();
+        }
+        $scope.bodyWidth = w.innerWidth() - scrollbarAdjust;
+        $scope.bodyHeight = w.innerHeight();
+        var columnCount = 8;
+        if ($scope.bodyWidth <= 768)
+        {
+            columnCount = 4;
+        }
+        else if ($scope.bodyWidth <= 1024)
+        {
+            columnCount = 6;
+        }
+        var columnsChanged = $scope.columnCount != columnCount;
+        $scope.columnCount = columnCount;
+        $scope.columnWidth = Math.floor($scope.bodyWidth / $scope.columnCount);
+        $scope.rowHeight = Math.floor($scope.columnWidth * (768 / 1024));
+        return columnsChanged;
+    }
 
     var maxRow = 0, lastRow = 0;
 
-    function tileSort(x, y) {
-        return y.size - x.size;
-    };
-
-    $scope.updateTiles = function () {
-        $scope.columnWidth = $scope.bodyWidth / $scope.columnCount;
-        $scope.rowHeight = $scope.columnWidth * (768 / 1024);
-        var i = 0, j = 0;
-        // update layout grid
-        for (i; i < $scope.grid.length; i++)
-        {
-            var row = $scope.grid.length[i];
-            row.height = $scope.rowHeight;
-            row.top = i * $scope.rowHeight;
-            for (j; j< row.columnCount; j++)
-            {
-                var col = row.columns[j];
-                col.top = row.top;
-                col.width = $scope.columnWidth;
-                col.left = j * $scope.columnWidth;
-                col.height = row.height;
-                if (col.tile)
-                {
-                    col.tile.top = col.top;
-                    col.tile.left = col.left;
-                    col.tile.height = col.height;
-                    col.tile.width = col.width;
+    $scope.updateTiles = function (columnsChanged) {
+        if (columnsChanged) {
+            var tiles = $scope.tiles;
+            $scope.grid = []; // dump the current layout, and re-layout the entire list
+            maxRow = 0;
+            lastRow = 0;
+            $scope.addTiles(tiles);
+        }
+        else {
+            var i = 0;
+            // update layout grid
+            for (i; i < $scope.grid.length; i++) {
+                var j = 0;
+                var row = $scope.grid[i];
+                row.height = $scope.rowHeight;
+                row.top = i * $scope.rowHeight;
+                for (j; j < row.columnCount; j++) {
+                    var col = row.columns[j];
+                    col.top = row.top;
+                    col.width = $scope.columnWidth;
+                    col.left = j * $scope.columnWidth;
+                    col.height = row.height;
+                    if (col.tile) {
+                        col.tile.top = col.top;
+                        col.tile.left = col.left;
+                        col.tile.height = col.height * col.tile.size;
+                        col.tile.width = col.width * col.tile.size;
+                        col.tile.element.css({
+                            top: col.tile.top,
+                            left: col.tile.left,
+                            width: col.tile.width,
+                            height: col.tile.height
+                        });
+                    }
                 }
             }
         }
+    };
+
+    function getScrollBarWidth() {
+        var inner = document.createElement('p');
+        inner.style.width = "100%";
+        inner.style.height = "200px";
+
+        var outer = document.createElement('div');
+        outer.style.position = "absolute";
+        outer.style.top = "0px";
+        outer.style.left = "0px";
+        outer.style.visibility = "hidden";
+        outer.style.width = "200px";
+        outer.style.height = "150px";
+        outer.style.overflow = "hidden";
+        outer.appendChild(inner);
+
+        document.body.appendChild(outer);
+        var w1 = inner.offsetWidth;
+        outer.style.overflow = 'scroll';
+        var w2 = inner.offsetWidth;
+        if (w1 == w2) w2 = outer.clientWidth;
+
+        document.body.removeChild(outer);
+
+        return (w1 - w2);
     };
 
     $scope.addTiles = function(tiles)
     {
-        tiles.sort(tileSort);
-        // algorithm:
-        // last row is stored between updates, and is not incremented until all columns are occupied, even if rows below it are fully occupied
-        // group by size, biggest to smallest
-        // place all large tiles, no more than 1 in same vertical section, rotating from left|center|right aligned, starting with last row
-        // place all medium tiles, starting from upper left of last row
-        // place all small tiles, starting from upper left of last row
-        // calculate last row
-        var large = [];
-        var med = [];
-        var small = [];
         var i = 0;
-        var largeSize = { w: $scope.columnWidth * 4, h: $scope.rowHeight * 4 };
-        var medSize = { w: $scope.columnWidth * 2, h: $scope.rowHeight * 2 };
-        var smallSize = { w: $scope.columnWidth, h: $scope.rowHeight };
-
+        
         for(i; i < tiles.length; i++)
         {
-            var tile = tiles[i];
-
-            if (tile.size == 1)
-                small.push(tile);
-            else if (tile.size == 2)
-                med.push(tile);
-            else large.push(tile);
-        }
-
-        var largeCount = 0;
-
-        i = 0;
-        for(i; i < large.length; i++)
-        {
-            placeLargeTile(large[i], largeCount % 3);
-            largeCount++;
+            placeTile(tiles[i]);
         }
     }
 
-    function placeTile(tile, position)
-    {
-        if (position == 0)
-            placeTileLeft(tile);
-        else if (position == 1)
-            placeTileCenter(tile);
-        else placeTileRight(tile);
-    }
-
-    function placeTileLeft(tile) {
+    function placeTile(tile) {
         var rowIndex = lastRow;
         var row = null;
+        var col = -1;
+        // find next vacant top 
         do
         {
-            row = $scope.grid[rowIndex];
+            row = $scope.grid[rowIndex]; // get the row
             if (!row)
-            {
+            {   // row doesn't exist, so create a new one, and initialize it
                 row = initRow(addRow($scope.columnCount), rowIndex);
-                $scope.grid.push(row);
+                $scope.grid.push(row); // put it into the binding $scope
             }
-            rowIndex++;
-        } while (row.columns[0].isOccupied);
-        var i = 0, j = 0;
+            col = tryPlaceTile(row, tile); // try to place the tile in this row
+            rowIndex++; // increment the row index pointer
+        } while (col == -1); // -1 means it won't fit on this row, try the next row
+
+        var i = 0;
         for(i; i < tile.size; i++)
         {
+            j = col; // start at the open column
             var r = $scope.grid[row.rowIndex + i];
             if (!r) {
+                // row doesn't exist, so add and init a new row
                 r = initRow(addRow($scope.columnCount), row.rowIndex + i);
                 $scope.grid.push(r);
             }
-            for (j; j < tile.size; j++) {
-                var col = r.columns[j];
-                if (j == 0 && i == 0)
+
+            for (j; j < col + tile.size; j++) {
+                var column = r.columns[j];
+                if (j == col && i == 0)
                 {
-                    col.tile = tile;
+                    // upper left corner cell is the tile's anchor position
+                    column.tile = tile;
                     tile.rowIndex = r.rowIndex;
                     tile.columnIndex = j;
                     tile.row = r;
+                    tile.column = column;
+                    tile.top = row.top;
+                    tile.left = column.left;
+                    tile.height = row.height * tile.size;
+                    tile.width = column.width * tile.size;
                 }
-                col.isOccupied = true;
+                column.isOccupied = true; // mark it full
             }
         }
+
+        lastRow = updateLastRow();
     }
 
+    function updateLastRow()
+    {
+        do {
+            var row = $scope.grid[lastRow];
 
-    function placeTileCenter(tile) {
+            if (!row) {   // row doesn't exist, so create a new one, and initialize it
+                row = initRow(addRow($scope.columnCount), lastRow);
+                $scope.grid.push(row); // put it into the binding $scope
+                return lastRow;
+            }
+
+            var j = 0;
+            for(j; j < row.columnCount; j++)
+            {
+                if (!row.columns[j].isOccupied)
+                    return lastRow;
+            }
+            lastRow++;
+        } while (true);
     }
 
-    function placeTileRight(tile) {
+    function tryPlaceTile(row, tile)
+    {
+        var i = 0, j = 0;
+        var col = 0;
+
+        // try each column in the row, based on tile's size
+        for (j; j < row.columnCount; j++) {
+            if (willFit(j, row, tile))
+            {
+                col = j;
+                return col; // the tile can be placed at this location
+            }
+        }
+
+        return -1; // the tile can't fit in this row
+    }
+
+    function willFit(col, row, tile)
+    {
+        if (col + tile.size <= row.columnCount) // if the tile is too wide, it will never fit, so just bail
+        {
+            var i = row.rowIndex;
+            for (i; i < row.rowIndex + tile.size; i++) {
+                var j = col;
+                var aRow = $scope.grid[i];
+                if (aRow) { // the row may not exist, if not, then we don't need to test it - it will be added later
+                    for (j; j < col + tile.size; j++) {
+                        if (aRow.columns[j].isOccupied) // if the column is filled, we bail
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        else return false; // too wide to fit from this column
+        
+        return true; // we never bailed, so it must fit at the given position
     }
 
     function initRow(row, rowIndex)
     {
         row.height = $scope.rowHeight;
         row.top = rowIndex * $scope.rowHeight;
-        for (j; j < row.columnCount; j++) {
+        row.rowIndex = rowIndex;
+        for (var j = 0; j < row.columnCount; j++) {
             var col = row.columns[j];
             col.top = row.top;
             col.width = $scope.columnWidth;
@@ -142,8 +238,8 @@
             if (col.tile) {
                 col.tile.top = col.top;
                 col.tile.left = col.left;
-                col.tile.height = col.height;
-                col.tile.width = col.width;
+                col.tile.height = col.height * tile.size;
+                col.tile.width = col.width * tile.size;
             }
         }
         return row;
@@ -159,27 +255,20 @@
         return true;
     }
 
-    var w = angular.element($window);
-    w.bind('resize', function () {
-        $scope.bodyWidth = w.innerWidth();
-        $scope.updateTiles($scope.bodyWidth);
-        $scope.$apply();
-    });
-
     $scope.loadMore = function ()
     {
-        $scope.bodyWidth = windowState.bodyWidth;
-        var tileCount = windowState.columnCount * 3;
+        var tileCount = Math.ceil($scope.columnCount * ($scope.bodyHeight / $scope.rowHeight));
         var last = $scope.tiles.length;
+        if (last > 0)
+        {
+            var row = $scope.grid[lastRow];
+            if ($scope.bodyHeight + w[0].pageYOffset < row.top) return;
+        }
         var tiles = [];
         for (var i = 1; i <= tileCount; i++) {
-            var size = 1;
+            var size = 2;
 
-            if (i % 3 == 0)
-            {
-                size = 2;
-            }
-            else if (i % 7 == 0)
+            if (i % 7 == 0)
             {
                 size = 4;
             }
@@ -190,15 +279,91 @@
                 caption: 'E plurubus unum',
                 subTitle: 'All for one',
                 href: '/' + i,
-                size: size
+                size: size,
+                id: last + i,
+                img: i % 3 == 0 
+                    ? 'https://c1.staticflickr.com/9/8438/7892937702_d84a1f7646_b.jpg'
+                    : i % 5 == 0
+                        ? 'http://files2.coloribus.com/files/adsarchive/part_1496/14966255/bud-light-beer-rescue-dog-600-89469.jpg'
+                        : 'http://i.ytimg.com/vi/fV9FjBBf1K8/maxresdefault.jpg'
             };
 
             tiles.push(tile);
             $scope.tiles.push(tile);
         }
-        $scope.updateTiles($scope.bodyWidth, tiles);
+
+        $scope.addTiles(tiles);
     };
-    
+
+    function isElementInViewport(el) {
+
+        //special bonus for those using jQuery
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+    }
+    var $selected = null;
+    $scope.show = function(tile)
+    {
+        var $element = $('#' + tile.id).parent();
+
+        var $clone = $($element.clone()[0].outerHTML).insertAfter($element);
+        $clone.attr('id', tile.id + '_clone_wrapper');
+        var $article = $($clone.children()[0]);
+        $article.attr('id', tile.id + '_clone');
+        $clone.tile = tile;
+        $selected = $clone;
+
+        var yOffset = window.pageYOffset;
+        var width = window.innerWidth - getScrollBarWidth();
+        var scale = width / tile.width;
+        var height = tile.height * scale;
+        var settings = {
+            queue: false,
+            duration: 0,
+            complete: function () {
+                $('body').css({ overflowY: 'hidden' });
+                $clone.click(function () {
+                    var $clone = $selected;
+                    var settings = {
+                        queue: false,
+                        duration: 0,
+                        complete: function () {
+                            $selected = null;
+                            $('body').css({ overflowY: 'auto' });
+                            $clone.css({ zindex: 0 });
+                            setTimeout(function () {
+                                $clone.remove();
+                            }, 200);
+                        }
+                    };
+                    $clone.animate({
+                        top: $clone.tile.top,
+                        left: $clone.tile.left,
+                        width: $clone.tile.width,
+                        height: $clone.tile.height
+                    }, settings);
+                });
+            }
+        };
+
+        $clone.css({ zIndex: 1000 });
+        $clone.animate({
+            top: yOffset,
+            left: 0,
+            width: width,
+            height: height
+        }, settings);
+    }
 
     function addRow(columnCount)
     {
@@ -227,6 +392,20 @@
                 height: null
             };
         }
+        maxRow = rowIndex;
         return row;
     }
-}]);
+}])
+.directive('tileRendered', function () {
+    return function (scope, element, attrs) {
+        var $element = $(element);
+        var tile = scope.tile;
+        tile.element = $element;
+        $element.css({
+            top: tile.top,
+            left: tile.left,
+            width: tile.width,
+            height: tile.height
+        });
+    };
+});
